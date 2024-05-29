b80b3eb5b3791f161262f40fb13aa3cdf6ff2ff0
Anuj-Wanjari <anujwanjari5@gmail.com>
2024-05-29 13:11:06 +0530

    Added icons to Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUser, faCode, faGraduationCap, faBriefcase, faProjectDiagram, faFileAlt } from '@fortawesome/free-solid-svg-icons';
 import './Navbar.css';
 
 function Navbar({ darkMode, toggleDarkMode }) {
@@ -7,13 +9,13 @@ function Navbar({ darkMode, toggleDarkMode }) {
     <nav className="navbar">
       <div className="logo">Portfolio</div>
       <ul className="nav-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/skills">Skills</Link></li>
-        <li><Link to="/education">Education</Link></li>
-        <li><Link to="/experience">Experience</Link></li>
-        <li><Link to="/projects">Projects</Link></li>
-        <li><Link to="/resume">Resume</Link></li>
+        <li><Link to="/"><FontAwesomeIcon icon={faHome} /> Home</Link></li>
+        <li><Link to="/about"><FontAwesomeIcon icon={faUser} /> About</Link></li>
+        <li><Link to="/skills"><FontAwesomeIcon icon={faCode} /> Skills</Link></li>
+        <li><Link to="/education"><FontAwesomeIcon icon={faGraduationCap} /> Education</Link></li>
+        <li><Link to="/experience"><FontAwesomeIcon icon={faBriefcase} /> Experience</Link></li>
+        <li><Link to="/projects"><FontAwesomeIcon icon={faProjectDiagram} /> Projects</Link></li>
+        <li><Link to="/resume"><FontAwesomeIcon icon={faFileAlt} /> Resume</Link></li>
         <li>
           <div className="toggle" onClick={toggleDarkMode}>
             <div className={darkMode ? 'toggle-circle dark' : 'toggle-circle'}></div>
